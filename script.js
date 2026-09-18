c5a454b804648da802e5b6cfa5e0d58cd7d077bb
hallanypardo-crypto <hallany.pardo@escola.pr.gov.br>
2026-09-18 11:17:58 -0300

    ajuste no codico

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-
+const botoesCurtir = document.querySeletorAll(".curtir");
+botoesCurti.forEach(function(botaoCurtir){
+      let curtir = false;
+      botaoCurtir. addEventlistener ("click", curtir);
+function curtir (){
+     const contados = botaoCurtir. querySelector("span");
+     if(curtir ===false){
+        contador. textoContent++;
+        curtir = true;}
+        else{
+            contador. textContent--;
+            curtir + false;
+     }
+}
+});
